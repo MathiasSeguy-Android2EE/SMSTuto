8445534c055a9279afae569b1185840e7233f186
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2019-06-05 17:00:20 +0200

    First commit after migration of may 2019

--- app/src/main/java/com/formation/androi2ee/mmxiv/aots/janv/MySMSService.java
+++ app/src/main/java/com/formation/androi2ee/mmxiv/aots/janv/MySMSService.java
@@ -6,8 +6,8 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
